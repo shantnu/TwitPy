34582feaca87f99860accf40d495709669e213cb
Shantnu Tiwari <a@a.com>
2015-08-07 14:23:14 +0100

    Creating classes, adding tests
    Tests wip

--- twit.py
+++ twit.py
@@ -8,6 +8,7 @@ import sqlite3
 from local_config import *
 import re
 
+db = "./twit_data.db"
 countries_list = ['Algeria', 'Angola', 'Benin', 'Botswana', 'Burkina', 'Burundi', 'Cameroon', 'Cape Verde', 'Central African Republic', 'Chad', 'Comoros', 'Congo', '"Congo', 'Djibouti', 'Egypt', 'Equatorial Guinea', 'Eritrea', 'Ethiopia', 'Gabon', 'Gambia', 'Ghana', 'Guinea', 'Guinea-Bissau', 'Ivory Coast', 'Kenya', 'Lesotho', 'Liberia', 'Libya', 'Madagascar', 'Malawi', 'Mali', 'Mauritania', 'Mauritius', 'Morocco', 'Mozambique', 'Namibia', 'Niger', 'Nigeria', 'Rwanda', 'Sao Tome and Principe', 'Senegal', 'Seychelles', 'Sierra Leone', 'Somalia', 'South Africa', 'South Sudan', 'Sudan', 'Swaziland', 'Tanzania', 'Togo', 'Tunisia', 'Uganda', 'Zambia', 'Zimbabwe', 'Afghanistan', 'Bahrain', 'Bangladesh', 'Bhutan', 'Brunei', 'Burma (Myanmar)', 'Cambodia', 'China', 'East Timor', 'India', 'Indonesia', 'Iran', 'Iraq', 'Israel', 'Japan', 'Jordan', 'Kazakhstan', '"Korea', '"Korea', 'Kuwait', 'Kyrgyzstan', 'Laos', 'Lebanon', 'Malaysia', 'Maldives', 'Mongolia', 'Nepal', 'Oman', 'Pakistan', 'Philippines', 'Qatar', 'Russian Federation', 'Saudi Arabia', 'Singapore', 'Sri Lanka', 'Syria', 'Tajikistan', 'Thailand', 'Turkey', 'Turkmenistan', 'United Arab Emirates', 'Uzbekistan', 'Vietnam', 'Yemen', 'Albania', 'Andorra', 'Armenia', 'Austria', 'Azerbaijan', 'Belarus', 'Belgium', 'Bosnia and Herzegovina', 'Bulgaria', 'Croatia', 'Cyprus', 'Czech Republic', 'Denmark', 'Estonia', 'Finland', 'France', 'Georgia', 'Germany', 'Greece', 'Hungary', 'Iceland', 'Ireland', 'Italy', 'Latvia', 'Liechtenstein', 'Lithuania', 'Luxembourg', 'Macedonia', 'Malta', 'Moldova', 'Monaco', 'Montenegro', 'Netherlands', 'Norway', 'Poland', 'Portugal', 'Romania', 'San Marino', 'Serbia', 'Slovakia', 'Slovenia', 'Spain', 'Sweden', 'Switzerland', 'Ukraine', 'United Kingdom', 'Vatican City', 'Antigua and Barbuda', 'Bahamas', 'Barbados', 'Belize', 'Canada', 'Costa Rica', 'Cuba', 'Dominica', 'Dominican Republic', 'El Salvador', 'Grenada', 'Guatemala', 'Haiti', 'Honduras', 'Jamaica', 'Mexico', 'Nicaragua', 'Panama', 'Saint Kitts and Nevis', 'Saint Lucia', 'Saint Vincent and the Grenadines', 'Trinidad and Tobago', 'United States', 'Australia', 'Fiji', 'Kiribati', 'Marshall Islands', 'Micronesia', 'Nauru', 'New Zealand', 'Palau', 'Papua New Guinea', 'Samoa', 'Solomon Islands', 'Tonga', 'Tuvalu', 'Vanuatu', 'Argentina', 'Bolivia', 'Brazil', 'Chile', 'Colombia', 'Ecuador', 'Guyana', 'Paraguay', 'Peru', 'Suriname', 'Uruguay', 'Venezuela']
 langs = \
 {'ar': 'Arabic',
@@ -67,7 +68,7 @@ class Twit_utils():
 
 
     def get_tweet_html(self, id):
-        oembed = api.get_oembed(id=id, hide_media = True, hide_thread = True)
+        oembed = self.api.get_oembed(id=id, hide_media = True, hide_thread = True)
 
         tweet_html = oembed['html'].strip("\n")
 
@@ -107,10 +108,12 @@ class stats():
 
 class listener(StreamListener):
 
-    def __init__(self, stats_obj, twit_utils):
+    def __init__(self, stats_obj, twit_utils, num_tweets_to_grab, retweet_count):
         self.count = 0
         self.stats_obj = stats_obj
         self.twit_utils = twit_utils
+        self.num_tweets_to_grab = num_tweets_to_grab
+        self.retweet_count = retweet_count
 
     def on_data(self, data):
         try:            
@@ -144,7 +147,7 @@ class listener(StreamListener):
 
             self.stats_obj.add_lang(langs[json_data["lang"]])
 
-            if retweet_count > 10000:
+            if retweet_count > self.retweet_count:
                 print (tweet, retweet_count, langs[json_data["lang"]])
                 self.stats_obj.add_top_lang(langs[json_data["lang"]])
                 tweet_html = self.twit_utils.get_tweet_html(json_data['id'])
@@ -152,7 +155,7 @@ class listener(StreamListener):
 
             self.count += 1
 
-            if self.count == 5000:
+            if self.count == self.num_tweets_to_grab:
                 return False
 
             return True
@@ -165,107 +168,93 @@ class listener(StreamListener):
 
     def on_error(self, status):
         print("IN on error")
-        pdb.set_trace()
+        #pdb.set_trace()
         print(status)
-        
-auth = tweepy.OAuthHandler(cons_tok, cons_sec)
-auth.set_access_token(app_tok, app_sec)
 
-api = tweepy.API(auth)
-twit_utils = Twit_utils(api)
+class TwitterMain():
+    def __init__(self, conn, num_tweets_to_grab, retweet_count):
+        '''
+        num_tweets_to_grab: The number of tweets to grab. If this number is too big, Twitter blocks you temporarily, so keep it small.
+        retweet_count: The number of times a tweet must have been retweeted for us to save it.
+        '''
+        self.auth = tweepy.OAuthHandler(cons_tok, cons_sec)
+        self.auth.set_access_token(app_tok, app_sec)
 
-db = "./twit_data.db"
+        self.api = tweepy.API(self.auth)
+        self.twit_utils = Twit_utils(self.api)
 
-conn = sqlite3.connect(db)
-c = conn.cursor()
+        self.conn = conn
+        self.c = self.conn.cursor()
 
-s = stats()
-try:
-    twitterStream = Stream(auth, listener(s, twit_utils))
-    twitterStream.sample()
-except Exception as e:
-        print("Error. Restarting Stream.... Error: ")
+        self.s = stats()
+
+        self.num_tweets_to_grab = num_tweets_to_grab
+        self.retweet_count = retweet_count
+
+
+    def get_streaming_data(self):
+        try:
+            twitterStream = Stream(self.auth, listener(self.s, self.twit_utils, self.num_tweets_to_grab, self.retweet_count))
+            twitterStream.sample()
+        except Exception as e:
+                print("Error. Restarting Stream.... Error: ")
+                print(e.__doc__)
+                #print(e.message)
+
+        lang, top_lang,love_words, swear_words, top_tweets, countries = self.s.get_stats()
+
+        print(Counter(lang))
+        print(Counter(top_lang))
+        print("Love Words {} Swear Words {}".format(love_words, swear_words))
+        print(Counter(countries))
+
+        self.c.execute("INSERT INTO lang_data VALUES (?,?, DATETIME('now'))", (str(list(Counter(lang).items())), str(list(Counter(top_lang).items()))))
+
+        self.c.execute("INSERT INTO love_data VALUES (?,?, DATETIME('now'))", (love_words, swear_words))
+
+        for t in top_tweets:
+            self.c.execute("INSERT INTO twit_data VALUES (?, DATETIME('now'))", (t,))
+
+        self.c.execute("INSERT INTO country_data VALUES (?, DATETIME('now'))", (str(list(Counter(countries).items())),))
+
+        self.conn.commit()
+
+
+
+    def get_trends(self):
+        trends = self.api.trends_place(1)
+        trend_data = []
+
+        for trend in trends[0]["trends"]:
+            #print(trend['name'])
+            trend_tweets = []
+            trend_tweets.append(trend['name'])
+            tt = tweepy.Cursor(self.api.search, q = trend['name']).items(3)
+         
+            for t in tt:
+                tweet_html = self.twit_utils.get_tweet_html(t.id)
+                trend_tweets.append(tweet_html)
+                print(tweet_html)
+
+            trend_data.append(tuple(trend_tweets))
+
+        self.c.executemany("INSERT INTO trend_data VALUES (?,?,?,?, DATETIME('now'))", trend_data)
+
+        self.conn.commit()
+
+
+if __name__ == "__main__":
+    num_tweets_to_grab = 2000
+    retweet_count = 10000
+    try:
+        conn = sqlite3.connect(db)
+        twit = TwitterMain(conn, num_tweets_to_grab, retweet_count)
+        #twit.get_streaming_data()
+        twit.get_trends()
+
+    except Exception as e:
         print(e.__doc__)
-        #print(e.message)
 
-lang, top_lang,love_words, swear_words, top_tweets, countries = s.get_stats()
+    finally:
+        conn.close()
 
-print(Counter(lang))
-print(Counter(top_lang))
-print("Love Words {} Swear Words {}".format(love_words, swear_words))
-print(Counter(countries))
-
-c.execute("INSERT INTO lang_data VALUES (?,?, DATETIME('now'))", (str(list(Counter(lang).items())), str(list(Counter(top_lang).items()))))
-
-c.execute("INSERT INTO love_data VALUES (?,?, DATETIME('now'))", (love_words, swear_words))
-
-for t in top_tweets:
-    c.execute("INSERT INTO twit_data VALUES (?, DATETIME('now'))", (t,))
-
-c.execute("INSERT INTO country_data VALUES (?, DATETIME('now'))", (str(list(Counter(countries).items())),))
-
-conn.commit()
-conn.close()
-
-
-#twitterStream.filter(languages=["en"])
-
-#tt = tweepy.Cursor(api.search, q = "python", since_id = 623488361030750208).items(5)
-
-#for t in tt:
-    #print(t.text)
-
-
-
-db = "./twit_data.db"
-
-conn = sqlite3.connect(db)
-c = conn.cursor()
-
-trends = api.trends_place(1)
-trend_data = []
-
-for trend in trends[0]["trends"]:
-    #print(trend['name'])
-    trend_tweets = []
-    trend_tweets.append(trend['name'])
-    tt = tweepy.Cursor(api.search, q = trend['name']).items(3)
-
-    
-
-    for t in tt:
-        tweet_html = twit_utils.get_tweet_html(t.id)
-        trend_tweets.append(tweet_html)
-        #print(tweet_html)
-
-    trend_data.append(tuple(trend_tweets))
-
-c.executemany("INSERT INTO trend_data VALUES (?,?,?,?, DATETIME('now'))", trend_data)
-
-
-
-conn.commit()
-conn.close()
-
-'''
-api.get_oembed(id=626415779202920448)
-Out[76]: 
-{'author_name': 'Cerise || Rabbit',
- 'author_url': 'https://twitter.com/AylinTheMuffin',
- 'cache_age': '3153600000',
- 'height': None,
- 'html': '<blockquote class="twitter-tweet"><p lang="de" dir="ltr">straub was bald <a href="https://twitter.com/hashtag/TodaysCubeFansWillNeverKnow?src=hash">#TodaysCubeFansWillNeverKnow</a></p>&mdash; kay (@frickgraser) <a href="https://twitter.com/frickgraser/status/626088782790443008">July 28, 2015</a></blockquote>\n<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>',
- 'provider_name': 'Twitter',
- 'provider_url': 'https://twitter.com',
- 'type': 'rich',
- 'url': 'https://twitter.com/AylinTheMuffin/statuses/626415779202920448',
- 'version': '1.0',
- 'width': 550}
-'''
-'''
-1. Most pop tweets
-2. Lang of all tweets as bar
-3. Lang of top tweets as pie 
-4.  Swear vs kind words as donut chart
-Something with gauge chart- cool! Maybe swear vs good- show in both donut and gague
-'''    
